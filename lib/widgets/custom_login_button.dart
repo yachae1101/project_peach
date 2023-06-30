3da55e7af7c927c2b7ab6a935296c680a5a9aa28
hyunseok99 <modesty666@g.hongik.ac.kr>
2023-07-01 04:10:04 +0900

    custom_login_button widget

---
+++ lib/widgets/custom_login_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:project_peach/widgets/palette.dart';
+
+class CustomLoginButton extends StatelessWidget {
+  final String text;
+  final Color backgroundColor;
+  final VoidCallback onPressed;
+
+  const CustomLoginButton({
+    required this.text,
+    required this.backgroundColor,
+    required this.onPressed,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 40.0),  // 기기마다 크기 다른지 체크 필요
+      height: 50.0,
+      width: MediaQuery.of(context).size.width-40,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}

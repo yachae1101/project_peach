e8fbb5cbb39241a193aabdb8b39a908d38718521
hyunseok99 <modesty666@g.hongik.ac.kr>
2023-07-06 14:52:25 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:project_peach/firebase_options.dart';
+import 'package:project_peach/providers/global_key.dart';
+import 'package:project_peach/screens/main/main_page.dart';
 import 'package:project_peach/screens/start/start_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => GlobalKeyOf(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,13 +26,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<GlobalKeyOf>(
+          create: (_) => GlobalKeyOf(),
+        ),
+      ],
+      child: MaterialApp(
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return MainPage();
+            } else {
+              return StartPage();
+            }
+          },
+        ),
       ),
-      home: StartPage(),
     );
   }
 }

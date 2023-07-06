71d2ba068cb9f8529d4d0a03a36b26b27af8e27a
hyunseok99 <modesty666@g.hongik.ac.kr>
2023-07-06 14:52:41 +0900

    palette

--- lib/widgets/palette.dart
+++ lib/widgets/palette.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Palette{
-  static const Color peachColor = Color(0xffffa19b);
-  static const Color textColor = Color(0xfa736F6E);
+  static const Color peachColor = Color(0xFFFFA19B);
+  static const Color peachAccentColor = Color(0xFFFD4264);
+  static const Color textColor = Color(0xFF797979);
+  static const Color backgroundColor = Color(0xFFFBF7F7);
+  static const Color primary = Color(0xFF9B4FFB);
+  static const Color secondary = Color(0xFFBC9EFD);
+  static const Color black = Color(0xFF1D1D1D);
+  static const Color grey = Color(0xFFE2E2E2);
+  static const Color lightGrey = Color(0xFFBFBFBF);
+  static const Color inactive = Color(0xFFE2E2E2);
+  static const Color red = Color(0xFFF35757);
+  static const Color green = Color(0xFF18CB2A);
 }
